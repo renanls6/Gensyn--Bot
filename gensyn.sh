cfef5f280f0ffd681a675328df9cc0016fa1cb6b
0x_Renan <142767050+renanls6@users.noreply.github.com>
2025-04-12 17:49:48 -0300

    Create gensyn.sh

---
+++ gensyn.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+WHITE='\033[1;37m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+# Display header
+display_header() {
+    clear
+    echo -e "${CYAN}"
+    echo -e " ${BLUE} ██████╗ ██╗  ██╗    ██████╗ ███████╗███╗   ██╗ █████╗ ███╗   ██╗${NC}"
+    echo -e " ${BLUE}██╔═████╗╚██╗██╔╝    ██╔══██╗██╔════╝████╗  ██║██╔══██╗████╗  ██║${NC}"
+    echo -e " ${BLUE}██║██╔██║ ╚███╔╝     ██████╔╝█████╗  ██╔██╗ ██║███████║██╔██╗ ██║${NC}"
+    echo -e " ${BLUE}████╔╝██║ ██╔██╗     ██╔══██╗██╔══╝  ██║╚██╗██║██╔══██║██║╚██╗██║${NC}"
+    echo -e " ${BLUE}╚██████╔╝██╔╝ ██╗    ██║  ██║███████╗██║ ╚████║██║  ██║██║ ╚████║${NC}"
+    echo -e " ${BLUE}╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝${NC}"
+    echo -e "${BLUE}=======================================================${NC}"
+    echo -e "${GREEN}       ✨ Bitz Setup Script ⛏️  ✨${NC}"
+    echo -e "${GREEN}       ✨ Follow me on X :https://x.com/renanls6  ✨${NC}"
+    echo -e "${BLUE}=======================================================${NC}"
+}
+
+echo "📦 Updating server..."
+sudo apt update && sudo apt install -y sudo
+
+echo "🔧 Installing required packages..."
+sudo apt update && sudo apt install -y \
+  python3 python3-venv python3-pip curl wget screen git lsof
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
+
+echo "🚀 Starting Gensyn script..."
+curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | bash
+
+echo "📁 Cloning repository..."
+cd $HOME
+[ -d rl-swarm ] && rm -rf rl-swarm
+git clone https://github.com/zunxbt/rl-swarm.git
+cd rl-swarm
+
+echo "📦 Updating Yarn..."
+cd modal-login
+yarn install
+yarn upgrade
+yarn add next@latest
+yarn add viem@latest
+
+echo "🚀 Installing Node..."
+cd $HOME/rl-swarm
+screen -dmS gensyn bash -c "python3 -m venv .venv && . .venv/bin/activate && ./run_rl_swarm.sh"
+
+echo -e "${GREEN}CC : CryptoAirdropHindi ${NC}"
+
+echo "Process completed."
+
+    echo -e "${CYAN}"
+    echo -e " ${BLUE} ██████╗ ██╗  ██╗    ██████╗ ███████╗███╗   ██╗ █████╗ ███╗   ██╗${NC}"
+    echo -e " ${BLUE}██╔═████╗╚██╗██╔╝    ██╔══██╗██╔════╝████╗  ██║██╔══██╗████╗  ██║${NC}"
+    echo -e " ${BLUE}██║██╔██║ ╚███╔╝     ██████╔╝█████╗  ██╔██╗ ██║███████║██╔██╗ ██║${NC}"
+    echo -e " ${BLUE}████╔╝██║ ██╔██╗     ██╔══██╗██╔══╝  ██║╚██╗██║██╔══██║██║╚██╗██║${NC}"
+    echo -e " ${BLUE}╚██████╔╝██╔╝ ██╗    ██║  ██║███████╗██║ ╚████║██║  ██║██║ ╚████║${NC}"
+    echo -e " ${BLUE}╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝${NC}"
+    echo -e "${BLUE}=======================================================${NC}"
+    echo -e "${GREEN}       ✨ Bitz Setup Script ⛏️  ✨${NC}"
+    echo -e "${GREEN}       ✨ Follow me on X :https://x.com/renanls6  ✨${NC}"
+    echo -e "${BLUE}=======================================================${NC}"
